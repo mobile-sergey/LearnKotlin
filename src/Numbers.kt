bbe9f72d0bc7abd29f9c76c45952d7a82b68d801
lavsexpert <lavrov-sergey@yandex.ru>
2020-08-13 01:18:41 +0300

    04.Numbers

---
+++ src/Numbers.kt
@@ -0,0 +1,24 @@
+fun main(args: Array<String>) {
+
+    val a = 12
+    val b = 25.3
+    val c = 25
+
+    // + - / *
+    println(b + a)
+    println(b - a)
+    println(b / a)
+    println(a * b)
+
+    println()
+
+    // + - / * %
+
+    println(c + a)
+    println(c - a)
+    println(c / a)
+    println(a * c)
+    println(c % a)
+
+    println()
+}
