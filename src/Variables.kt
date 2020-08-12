b0e54fb0d4ffa62c88dccf8b2f8c29ea1f11f84b
lavsexpert <lavrov-sergey@yandex.ru>
2020-08-13 00:50:26 +0300

    02.Variables

---
+++ src/Variables.kt
@@ -0,0 +1,30 @@
+fun main(args: Array<String>) {
+    val name = "Sergey"
+    //name = "Superman"
+
+    var isGood = true
+    isGood = false
+
+    println("Is " + name + " good? The answer is : " + isGood)
+
+    var a = 3
+    var b = 6
+    println(a + b)
+
+    val doubldeNum = 123.45 // 64 bit
+    val floatNum = 123.45f  // 32 bit
+
+    val longNum = 1234567890123123L // 64 bit
+    val aDouble = a.toString()
+
+    val newName: String = "JonnyB"
+    val c: Int = 9
+    val d: Double = 123.45
+    val f: Float = 123.45f
+    var l: Long = 1212312341234123L
+
+    var hero: String
+    hero = "Batman"
+    hero = "Superman"
+    println(hero)
+}
