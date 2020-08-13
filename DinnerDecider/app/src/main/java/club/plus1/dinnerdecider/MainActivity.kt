f3935d731d86b3f3506580f51b231b583ed2f077
lavsexpert <lavrov-sergey@yandex.ru>
2020-08-13 04:18:18 +0300

    13.App: Dinner decider - finish this app

--- DinnerDecider/app/src/main/java/club/plus1/dinnerdecider/MainActivity.kt
+++ DinnerDecider/app/src/main/java/club/plus1/dinnerdecider/MainActivity.kt
@@ -1,16 +1,29 @@
 package club.plus1.dinnerdecider
 
-import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("SetTextI18n")
+
+    val foodList = arrayListOf("Chines", "Hamburger", "Pizza", "McDonalds")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        selectedFoodText.text = "Burger King"
+        decideButton.setOnClickListener {
+            val random = Random()
+            val randomFood = random.nextInt(foodList.count())
+            selectedFoodText.text = foodList[randomFood]
+        }
+
+        addFoodButton.setOnClickListener {
+            val newFood = addFoodText.text.toString()
+            foodList.add(newFood)
+            addFoodText.text.clear()
+            println(foodList)
+        }
     }
 }
