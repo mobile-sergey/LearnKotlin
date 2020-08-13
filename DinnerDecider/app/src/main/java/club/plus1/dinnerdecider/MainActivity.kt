d3bdda192a461e3fe43ac073d5fe11187c274f98
lavsexpert <lavrov-sergey@yandex.ru>
2020-08-13 04:04:47 +0300

    13.App: Dinner decider - finish layout and begin to write code

--- DinnerDecider/app/src/main/java/club/plus1/dinnerdecider/MainActivity.kt
+++ DinnerDecider/app/src/main/java/club/plus1/dinnerdecider/MainActivity.kt
@@ -1,11 +1,16 @@
 package club.plus1.dinnerdecider
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        selectedFoodText.text = "Burger King"
     }
 }
